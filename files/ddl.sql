3c9d09115a6f52c8c389a7799b6f62542f416c83
klashxx <klashxx@gmail.com>
2016-12-15 16:42:52 +0100

    sql ordered bt column

---
+++ files/ddl.sql
@@ -0,0 +1,20 @@
+CREATE TABLE BBBBBB
+(
+   ID bigint NOT NULL
+   NAME varchar(50)
+   CURRENCY varchar(3)
+   SYS_START timestamp NOT NULL
+   SYS_END timestamp NOT NULL
+   TS_ID timestamp NOT NULL
+   EXPORT int
+)
+
+CREATE TABLE AA
+(
+   ID bigint NOT NULL
+   SYS_START timestamp NOT NULL
+   SYS_END timestamp NOT NULL
+   TS_ID timestamp NOT NULL
+   NAME varchar(100) NOT NULL
+)
+
---
+++ sql_order.awk
@@ -0,0 +1,17 @@
+# SQL order just want columns inside the brackets.
+# Example: awk -f sql_order.awk files/ddl.sql
+
+BEGIN{
+  PROCINFO["sorted_in"] = "@ind_str_asc"
+  f=1
+  }
+/^\(/{f=0;print;next}
+/^\)/{for (i in a){
+       print a[i]
+       }
+     delete a
+     print
+     f=1
+     next}
+!f{a[$1]=$0}
+f{print}
